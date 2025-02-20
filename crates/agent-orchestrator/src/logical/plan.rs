d003eac6f71178474821723dbe501c67f2e9b16d
Jack Eadie <jack.eadie0@gmail.com>
2025-02-20 16:10:13 +0900

    Attempt to create logical plan again if JSONSchema issue (#564)

--- crates/agent-orchestrator/src/logical/plan.rs
+++ crates/agent-orchestrator/src/logical/plan.rs
@@ -6,6 +6,7 @@ use async_openai::{
     },
 };
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use uuid::Uuid;
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -53,8 +54,18 @@ pub enum Action {
 impl LogicalPlan {
     pub fn new(body: &str) -> Result<Self, serde_json::Error> {
         let mut plan: LogicalPlan = serde_json::from_str(body)?;
+        plan.add_uuids();
+        Ok(plan)
+    }
 
-        plan.tasks.iter_mut().for_each(|task| {
+    pub fn from_value(body: Value) -> Result<Self, serde_json::Error> {
+        let mut plan: LogicalPlan = serde_json::from_value(body)?;
+        plan.add_uuids();
+        Ok(plan)
+    }
+
+    fn add_uuids(&mut self) {
+        self.tasks.iter_mut().for_each(|task| {
             task.steps.iter_mut().for_each(|step| {
                 if step.uuid.is_none() {
                     step.uuid = Some(Uuid::new_v4());
@@ -65,20 +76,30 @@ impl LogicalPlan {
                 task.uuid = Some(Uuid::new_v4());
             }
         });
-
-        Ok(plan)
     }
 
     pub fn from_chat_completion(
         completion: &CreateChatCompletionResponse,
-    ) -> Result<Self, anyhow::Error> {
+    ) -> Result<Self, ConversionError> {
         let body = completion
             .choices
             .first()
-            .and_then(|choice| choice.message.content.as_ref())
-            .ok_or_else(|| anyhow::anyhow!("No content in the response"))?;
+            .and_then(|choice| choice.message.content.clone())
+            .unwrap_or_default();
 
-        Ok(Self::new(body)?)
+        let as_value = serde_json::from_str(body.as_str()).map_err(ConversionError::SerdeJson)?;
+
+        // First we validate against JSONSchema so the error message is more precise and informative.
+        let yaml_value: serde_json::Value =
+            serde_yaml::from_str(include_str!("openai_response_format.yaml"))
+                .map_err(ConversionError::SerdeYaml)?;
+
+        let v = jsonschema::validator_for(&yaml_value["json_schema"]["schema"])
+            .map_err(|e| ConversionError::JsonSchema(e.to_owned()))?;
+        v.validate(&as_value)
+            .map_err(|e| ConversionError::JsonSchema(e.to_owned()))?;
+
+        Self::from_value(as_value).map_err(ConversionError::SerdeJson)
     }
 
     pub fn to_chat_request(&self) -> Result<CreateChatCompletionRequest, OpenAIError> {
@@ -87,6 +108,23 @@ impl LogicalPlan {
             .messages(vec![ChatCompletionRequestMessage::User(body.into())])
             .build()?;
         Ok(req)
+    }
+}
+
+#[derive(Debug)]
+pub enum ConversionError {
+    SerdeJson(serde_json::Error),
+    SerdeYaml(serde_yaml::Error),
+    JsonSchema(jsonschema::ValidationError<'static>),
+}
+
+impl std::fmt::Display for ConversionError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ConversionError::SerdeJson(e) => write!(f, "{e}"),
+            ConversionError::JsonSchema(e) => write!(f, "{e}"),
+            ConversionError::SerdeYaml(e) => write!(f, "{e}"),
+        }
     }
 }
 
