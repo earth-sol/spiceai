b705689bb9fb9b5960a541f3d63f2f83a59113c4
Luke Kim <80174+lukekim@users.noreply.github.com>
2025-02-27 12:25:14 +0900

    Remove objective

--- crates/test-framework/src/spicepod_utils/mod.rs
+++ crates/test-framework/src/spicepod_utils/mod.rs
@@ -38,7 +38,6 @@ pub fn load_spicepod(path: PathBuf) -> anyhow::Result<SpicepodDefinition> {
 pub fn from_app(app: App) -> SpicepodDefinition {
     SpicepodDefinition {
         name: app.name,
-        objective: None,
         runtime: app.runtime,
         extensions: app.extensions,
         secrets: app.secrets,
