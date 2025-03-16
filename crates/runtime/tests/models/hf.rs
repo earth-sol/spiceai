e120f26a8c57eb7c97398d0d3095c665ebaf5a40
jeadie <jack@spice.ai>
2025-03-16 15:52:53 +1000

    reenable runtime/tests/models/hf.rs

--- crates/runtime/tests/models/hf.rs
+++ crates/runtime/tests/models/hf.rs
@@ -431,7 +431,6 @@ async fn huggingface_test_embeddings() -> Result<(), anyhow::Error> {
 }
 
 #[tokio::test]
-#[ignore] // https://github.com/spiceai/spiceai/issues/4943
 async fn huggingface_test_chat_completion() -> Result<(), anyhow::Error> {
     let _tracing = init_tracing(None);
 
@@ -495,7 +494,6 @@ async fn huggingface_test_chat_completion() -> Result<(), anyhow::Error> {
 }
 
 #[tokio::test]
-#[ignore] // https://github.com/spiceai/spiceai/issues/4943
 async fn huggingface_test_chat_messages() -> Result<(), anyhow::Error> {
     if HF_TEST_MODEL_REQUIRES_HF_API_KEY {
         verify_env_secret_exists("SPICE_HF_TOKEN")
