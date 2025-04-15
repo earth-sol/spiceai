607643cf6d4ca1b15148adc198171bd60129a482
Qianqian <130200611+Sevenannn@users.noreply.github.com>
2025-04-15 14:09:43 +0800

    Refactor accelerator registry as part of runtime. (#5318)
    * refactor accelerator registry as part of runtime
    
    * Fix make test
    
    * Update integration tests
    
    * resolve merge conflicts
    
    * Use AcceleratorRegistry type
    
    * Fix linter
    
    * resolve merge conflict
    
    * fix iceberg integration test
    
    * fix
    
    * Don't allow user to customize accelerator registry in Runtime initialization
    
    * wip on callback
    
    * WIP: Use callback for Datafusion configuration
    
    * configure datafusion instead of config option
    
    * fmt
    
    * WIP: refactor accelerator engine as a struct
    
    * WIP: Refactor Dataset to conatin Arc<Runtime>
    
    * fix integration test and make test
    
    * fix: linter
    
    * apply linter fix
    
    * fix integration test
    
    * apply feedbacks: use arc, remove commented out code, use function pointer types, remove ACCELERATION_MUTEX, fix Debug derive
    
    * Make runtime & app required for Dataset, make accelerator_engine_registry Arc, new struct datasetbuilder
    
    * fix debug, fix error, remove unnecessary  accelerator registry from methods
    
    * Fix integration tests
    
    * cleanup
    
    * Make different duckdb acceleration instances in integration test
    
    * update remove_dataset, use rwlock for accelerator_engine_registry
    
    * Improvements to error messages
    
    ---------
    
    Co-authored-by: Phillip LeBlanc <phillip@leblanc.tech>

--- crates/runtime/src/lib.rs
+++ crates/runtime/src/lib.rs
@@ -23,6 +23,7 @@ use std::time::Duration;
 use std::{collections::HashMap, sync::Arc};
 use tokio::task::JoinHandle;
 
+use crate::dataaccelerator::AcceleratorEngineRegistry;
 use crate::{
     auth::EndpointAuth, dataconnector::DataConnector, datafusion::DataFusion,
     internal_table::Error as InternalTableError, model::ENABLE_MODEL_SUPPORT_MESSAGE,
@@ -53,6 +54,7 @@ use snafu::prelude::*;
 use spicepod::component::eval::Eval;
 use status::ComponentStatus;
 use tls::TlsConfig;
+
 use tokio::sync::{RwLock, oneshot::error::RecvError};
 use tokio_util::sync::CancellationToken;
 use tools::factory::default_available_catalogs;
@@ -297,6 +299,12 @@ pub enum Error {
     #[snafu(display("Unable to create directory: {source}"))]
     UnableToCreateDirectory { source: std::io::Error },
 
+    #[snafu(display("Unable to build dataset: {dataset}: {source}"))]
+    UnableToBuildDataset {
+        dataset: String,
+        source: crate::component::dataset::Error,
+    },
+
     #[snafu(display("{source}"))]
     ComponentError { source: component::Error },
 
@@ -346,6 +354,7 @@ pub struct Runtime {
     status: Arc<status::RuntimeStatus>,
 
     runtime_tasks: Arc<RwLock<HashMap<String, CancellableTaskHandle>>>,
+    accelerator_engine_registry: Arc<AcceleratorEngineRegistry>,
 }
 
 impl Runtime {
@@ -379,8 +388,13 @@ impl Runtime {
         Arc::clone(&self.app)
     }
 
+    #[must_use]
+    pub fn accelerator_engine_registry(&self) -> Arc<AcceleratorEngineRegistry> {
+        Arc::clone(&self.accelerator_engine_registry)
+    }
+
     /// Requests a loaded extension, or will attempt to load it if part of the autoloaded extensions.
-    pub async fn extension(&self, name: &str) -> Option<Arc<dyn Extension>> {
+    pub async fn extension(self: Arc<Self>, name: &str) -> Option<Arc<dyn Extension>> {
         let extensions = self.extensions.read().await;
 
         if let Some(extension) = extensions.get(name) {
@@ -392,12 +406,12 @@ impl Runtime {
             let mut extensions = self.extensions.write().await;
             let mut extension = autoload_factory.create();
             let extension_name = extension.name().to_string();
-            if let Err(err) = extension.initialize(self).await {
+            if let Err(err) = extension.initialize(self.as_ref()).await {
                 tracing::error!("Unable to initialize extension {extension_name}: {err}");
                 return None;
             }
 
-            if let Err(err) = extension.on_start(self).await {
+            if let Err(err) = extension.on_start(Arc::clone(&self)).await {
                 tracing::error!("Unable to start extension {extension_name}: {err}");
                 return None;
             }
@@ -421,7 +435,8 @@ impl Runtime {
         tls_config: Option<Arc<TlsConfig>>,
         endpoint_auth: EndpointAuth,
     ) -> Result<()> {
-        self.register_metrics_table(self.prometheus_registry.is_some())
+        Arc::clone(&self)
+            .register_metrics_table(self.prometheus_registry.is_some())
             .await?;
 
         // Start Http server
@@ -551,13 +566,13 @@ impl Runtime {
     }
 
     /// Updates all of the component statuses to `Initializing`.
-    pub async fn set_components_initializing(&self) {
+    pub async fn set_components_initializing(self: Arc<Self>) {
         let app_lock = self.app.read().await;
         let Some(app) = app_lock.as_ref() else {
             return;
         };
 
-        let valid_datasets = Self::get_valid_datasets(app, LogErrors(false));
+        let valid_datasets = Arc::clone(&self).get_valid_datasets(app, LogErrors(false));
         for ds in &valid_datasets {
             self.status
                 .update_dataset(&ds.name, ComponentStatus::Initializing);
@@ -591,7 +606,7 @@ impl Runtime {
                 .update_catalog(&catalog.name, ComponentStatus::Initializing);
         }
 
-        let valid_views = Self::get_valid_views(app, LogErrors(false));
+        let valid_views = Arc::clone(&self).get_valid_views(app, LogErrors(false));
         for view in valid_views {
             self.status
                 .update_view(&view.name, ComponentStatus::Initializing);
@@ -604,9 +619,9 @@ impl Runtime {
     /// This includes waiting for the first refresh of any accelerated tables to complete.
     #[allow(clippy::too_many_lines)]
     pub async fn load_components(self: Arc<Self>) {
-        self.set_components_initializing().await;
+        Arc::clone(&self).set_components_initializing().await;
 
-        self.start_extensions().await;
+        Arc::clone(&self).start_extensions().await;
 
         // Must be loaded before datasets
         self.load_embeddings().await;
@@ -647,7 +662,8 @@ impl Runtime {
             async move {
                 self_clone.load_models().await;
 
-                let app_lock = self_clone.app.read().await;
+                let app_ref = Arc::clone(&self_clone).app();
+                let app_lock = app_ref.read().await;
 
                 if !cfg!(feature = "models")
                     && app_lock.as_ref().is_some_and(|s| !s.evals.is_empty())
@@ -662,12 +678,15 @@ impl Runtime {
                     self_clone.load_eval_scorer().await;
                     let () = self_clone.verify_evals().await;
                     let an_eval_exists = app_lock.as_ref().is_some_and(|app| !app.evals.is_empty());
-                    if !an_eval_exists {
+                    if an_eval_exists {
+                        drop(app_lock);
+                        if let Err(err) = self_clone.load_eval_tables().await {
+                            tracing::warn!("Failed to create internal eval tables: {err}");
+                        }
+                    } else {
                         tracing::trace!(
                             "No eval spice components defined. Therefore not loading eval tables into database."
                         );
-                    } else if let Err(err) = self_clone.load_eval_tables().await {
-                        tracing::warn!("Creating internal eval run table: {err}");
                     }
                 }
             }
@@ -777,7 +796,7 @@ impl Runtime {
         self.df.shutdown().await;
         dataconnector::unregister_all().await;
         catalogconnector::unregister_all().await;
-        dataaccelerator::unregister_all().await;
+        self.accelerator_engine_registry.unregister_all().await;
         tools::factory::unregister_all_factories().await;
         document_parse::unregister_all().await;
 
