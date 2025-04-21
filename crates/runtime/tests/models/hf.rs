984935c6a08a70d3a3c5cd524bc67bd8f561a6d9
Sevenannn <qianqliu@uw.edu>
2025-04-21 15:46:40 +0800

    wait until runtime is ready before sending request

--- crates/runtime/tests/models/hf.rs
+++ crates/runtime/tests/models/hf.rs
@@ -506,7 +506,7 @@ async fn huggingface_test_chat_messages() -> Result<(), anyhow::Error> {
 
     test_request_context().scope(async {
         let model = Arc::new(create_hf_model(
-            "huggingface:meta-llama/Llama-3.2-1B-Instruct",
+            "huggingface:meta-llama/Llama-3.2-3B-Instruct",
         Some("llama"),
         None,
             None,
@@ -527,6 +527,8 @@ async fn huggingface_test_chat_messages() -> Result<(), anyhow::Error> {
             }
             () = Arc::clone(&rt).load_components() => {}
         }
+
+        runtime_ready_check(&rt).await;
 
         let tool_model = Box::new(ToolUsingChat::new(
             Arc::clone(&model),
