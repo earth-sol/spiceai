4bc71f893940ea2769572738e7aedb24bc1793e2
jeadie <jack@spice.ai>
2025-04-28 16:58:50 +1000

    utils for test-framework for Flight SQL

--- crates/test-framework/src/flight/mod.rs
+++ crates/test-framework/src/flight/mod.rs
@@ -14,10 +14,18 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+use std::sync::Arc;
+
 use anyhow::Result;
-use arrow::record_batch::RecordBatch;
+use arrow::{
+    array::{ArrayRef, StringArray},
+    datatypes::{Field, FieldRef, Fields, Schema},
+    record_batch::RecordBatch,
+};
+use arrow_flight::sql::client::FlightSqlServiceClient;
 use flight_client::FlightClient;
-use futures::StreamExt;
+use futures::{StreamExt, TryStreamExt};
+use tonic::transport::Channel;
 
 /// Query a flight client and return the result as a vector of record batches
 ///
@@ -39,4 +47,98 @@ pub async fn put_batches(
     batches: Vec<RecordBatch>,
 ) -> Result<()> {
     Ok(client.publish(dataset_path, batches).await?)
+}
+
+/// Query a prepared statement against a FlightSQL compatible server.
+///
+/// To construct `parameters` as a [`RecordBatch`], see [`create_param_batch`].
+async fn execute_prepared_statement(
+    client: &mut FlightSqlServiceClient<Channel>,
+    query: &str,
+    parameters: RecordBatch,
+) -> Result<Vec<RecordBatch>, anyhow::Error> {
+    let mut prepared_stmt = client.prepare(query.to_string(), None).await?;
+
+    prepared_stmt.set_parameters(parameters)?;
+
+    let flight_info = prepared_stmt.execute().await?;
+
+    let mut results = Vec::new();
+
+    for endpoint in &flight_info.endpoint {
+        let ticket = endpoint
+            .ticket
+            .as_ref()
+            .ok_or_else(|| anyhow::anyhow!("No ticket in endpoint"))?;
+
+        let stream = client.do_get(ticket.clone()).await?;
+        let mut batch_results: Vec<RecordBatch> = stream.try_collect().await?;
+
+        results.append(&mut batch_results);
+    }
+
+    Ok(results)
+}
+
+pub struct PreparedStatementParamColumn<'a> {
+    name: &'a str,
+    dtype: arrow::datatypes::DataType,
+    nullable: bool,
+    array: ArrayRef,
+}
+
+impl<'a> PreparedStatementParamColumn<'a> {
+    pub fn new(
+        name: &'a str,
+        dtype: arrow::datatypes::DataType,
+        nullable: bool,
+        array: ArrayRef,
+    ) -> Self {
+        Self {
+            name,
+            dtype,
+            nullable,
+            array,
+        }
+    }
+}
+
+/// # Usage
+///
+/// ```rust
+/// create_param_batch(vec![
+///   PreparedStatementParamColumn::new(
+///     "$1",
+///     arrow::datatypes::DataType::Int64,
+///     false,
+///     Arc::new(Int64Array::from(vec![41])) as Arc<dyn arrow::array::Array>
+///   ),
+///   PreparedStatementParamColumn::new(
+///     "$2",
+///     arrow::datatypes::DataType::Utf8,
+///     true,
+///     Arc::new(StringArray::from(vec![Some(41), 42])) as Arc<dyn arrow::array::Array>
+///   )
+/// ])?;
+/// ```
+fn create_param_batch(
+    params: Vec<PreparedStatementParamColumn>,
+) -> Result<RecordBatch, anyhow::Error> {
+    let (fields, columns): (Vec<FieldRef>, Vec<ArrayRef>) = params
+        .iter()
+        .map(|col| {
+            let PreparedStatementParamColumn {
+                name,
+                dtype,
+                nullable,
+                array,
+            } = col.clone();
+            (
+                Arc::new(Field::new(name.clone(), dtype.clone(), *nullable)),
+                Arc::clone(&array),
+            )
+        })
+        .unzip();
+
+    RecordBatch::try_new(Arc::new(Schema::new(fields)), columns).map_err(Into::into)
 }
